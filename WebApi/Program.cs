37721d62b1fdffe4e85721d7823be69d1688f2a1
MagdaSroka <138120680+Sroczysk0@users.noreply.github.com>
2025-04-15 15:17:59 +0200

    pierwsza1V

---
+++ WebApi/Program.cs
@@ -0,0 +1,31 @@
+using Infrastructure.EF;
+using Microsoft.AspNetCore.Identity;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddIdentity<UserEntity, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapScalarApiReference();
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
